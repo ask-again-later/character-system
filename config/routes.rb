398aaa7431c4341b3f192dc976f08e2cc4e21bf6
cmho <carlymho@gmail.com>
2017-02-13 00:17:57 -0600

    Add editing/adding interface for true selves

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   namespace :data_entry do
     resources :advantages
     resources :challenges
+    resources :true_selves
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
