9e8b3e9c1da254169eb3b3c7ef34a009b647a1a3
Korey Enright <kenright19@gmail.com>
2017-03-02 21:40:47 -0600

    Makes question answer model comply with tests

--- app/models/questionnaire_answer.rb
+++ app/models/questionnaire_answer.rb
@@ -1,4 +1,8 @@
 class QuestionnaireAnswer < ApplicationRecord
 	belongs_to :character
 	belongs_to :questionnaire_item
+
+	validates :questionnaire_item, presence: true
+	validates :character, 				 presence: true
+	validates :answer, 						 presence: true
 end
