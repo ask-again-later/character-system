5240b943c2c930ab3ad9ff176a03cf0692a905b2
Korey Enright <kenright19@gmail.com>
2017-03-02 19:41:52 -0600

    Adds a question answer test

--- test/models/questionnaire_answer_test.rb
+++ test/models/questionnaire_answer_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class QuestionnaireAnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "question answers are valid with user and question" do
+    test_answer = QuestionnaireAnswer.new(questionnaire_item: questionnaire_items(:one), character: characters(:one), answer: "a thing lol")
+    assert test_answer.valid?
+  end
 end
