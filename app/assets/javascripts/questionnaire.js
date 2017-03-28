df1837b604a34e96ab5e412ecb619af438ee2417
cmho <carlymho@gmail.com>
2017-03-27 20:41:22 -0500

    fix javascript on new/edit page

--- app/assets/javascripts/questionnaire.js
+++ app/assets/javascripts/questionnaire.js
@@ -10,25 +10,16 @@ $(document).ready(function() {
     }
   });
 
-  $('#questionnaire').nestedSortable({
+  $('ul#questionnaire').sortable({
     handle: 'label.handle',
-    listType: 'ul',
-    items: 'li',
-    toleranceElement: '.container',
-    maxLevels: 2,
-    protectRoot: true,
     stop: function() {
-      var sectionId;
-      $('#questionnaire > li').each(function (index) {
+      $('#questionnaire li').each(function (index) {
         $(this).find('.order-field').val(index);
-        $(this).find('ul').children().each(function(subindex) {
-          $(this).find('.order-field').val(subindex);
-        })
       });
     }
   });
 
-  $('#questionnaire').delegate('.add-question', 'click', function(e) {
+  $('ul#questionnaire').delegate('.add-question', 'click', function(e) {
     e.preventDefault();
     var $newItem = $('.question-template').children().clone();
     var $list = $(this).closest('li').find('ul');
@@ -36,22 +27,7 @@ $(document).ready(function() {
     $('#questionnaire').trigger('stop');
   });
 
-  $('.add-section').click(function(e) {
-    e.preventDefault();
-    var $newItem = $('.section-template').children().clone();
-    $('#questionnaire').append($newItem);
-    $('#questionnaire').trigger('stop');
-  });
-
-  $('#questionnaire').delegate('li > .container.top > .delete', 'click', function() {
-    var conf = confirm('Are you sure you want to delete this section? This will also delete all questions within the section.');
-    if (conf === true) {
-      $(this).closest('li').remove();
-    }
-    return;
-  });
-
-  $('#questionnaire').delegate('li > .container ul > li .delete', 'click', function() {
+  $('#questionnaire').delegate('.delete', 'click', function() {
     var conf = confirm('Are you sure you want to delete this question?');
     if (conf === true) {
       $(this).closest('li').remove();
