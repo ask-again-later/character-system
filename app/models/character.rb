002590a18f9f2942e1d2199be034f9baff225b9a
cmho <carlymho@gmail.com>
2017-04-01 09:12:53 -0500

    Switch to .destroy rather than .delete and set dependent models to cascade delete

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,9 @@
 class Character < ApplicationRecord
 	belongs_to :user
-	has_many :questionnaire_answers
-	has_many :character_has_challenges
+	has_many :questionnaire_answers, dependent: :destroy
+	has_many :character_has_challenges, dependent: :destroy
 	has_many :challenges, through: :character_has_challenges
-	has_many :character_has_advantages
+	has_many :character_has_advantages, dependent: :destroy
 	has_many :advantages, through: :character_has_advantages
 	belongs_to :true_self
 
