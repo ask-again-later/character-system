7490d1411d177971b7c3039c14c23dc29bcb23a7
cmho <carlymho@gmail.com>
2017-04-01 11:47:05 -0500

    make the styling of choosing mechanics/non-mechanics prettier

--- app/assets/javascripts/wizard.js
+++ app/assets/javascripts/wizard.js
@@ -1,30 +1,36 @@
-$('#back').on('click', function(e) {
-  e.preventDefault();
-  $('#form-action').val('save-back');
-  $('form').submit();
-});
+$(document).ready(function() {
+  $('.build-method label').on('click', function() {
+    $(this).addClass('checked').siblings('.checked').removeClass('checked');
+  });
 
-$('#save').on('click', function(e) {
-  e.preventDefault();
-  $('#form-action').val('save');
-  $('form').submit();
-});
+  $('#back').on('click', function(e) {
+    e.preventDefault();
+    $('#form-action').val('save-back');
+    $('form').submit();
+  });
 
-$('#save-continue').on('click', function(e) {
-  e.preventDefault();
-  $('#form-action').val('save-continue');
-  $('form').submit();
-});
+  $('#save').on('click', function(e) {
+    e.preventDefault();
+    $('#form-action').val('save');
+    $('form').submit();
+  });
 
-$('#save-skip').on('click', function(e) {
-  e.preventDefault();
-  $('#form-action').val('save-skip');
-  $('form').submit();
-});
+  $('#save-continue').on('click', function(e) {
+    e.preventDefault();
+    $('#form-action').val('save-continue');
+    $('form').submit();
+  });
 
-$('#switch-non-mechanics').on('click', function(e) {
-  e.preventDefault();
-  $('#character_use_extended').val('true');
-  $('#form-action').val('switch-non-mechanics');
-  $('form').submit();
+  $('#save-skip').on('click', function(e) {
+    e.preventDefault();
+    $('#form-action').val('save-skip');
+    $('form').submit();
+  });
+
+  $('#switch-non-mechanics').on('click', function(e) {
+    e.preventDefault();
+    $('#character_use_extended').val('true');
+    $('#form-action').val('switch-non-mechanics');
+    $('form').submit();
+  });  
 });
