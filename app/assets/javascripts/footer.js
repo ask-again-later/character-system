a14a564f0363f726f74a3531d7aa40d6ec9a657a
cmho <carlymho@gmail.com>
2017-04-02 22:45:41 -0500

    move questionnaire accordion js to footer

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
 
   $('textarea').wrap('<div class="textarea-wrapper"></div>');
   $('.textarea-wrapper').append('<span class="notice">You can use <a href="https://guides.github.com/features/mastering-markdown/" rel="external" target="_blank">Markdown</a> in this area.</span>');
+
+  $('.questionnaire h4').on('click', function() {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open');
+      $(this).next('.section-content').slideUp();
+    } else {
+      $(this).addClass('open');
+      $(this).next('.section-content').slideDown();
+    }
+  });
+
+  $('.questionnaire h4:first-of-type').click();
 });
