9fe3d1aa991a4dc4a12f2f269ca43f67c716f9b2
Carly Ho <cho@cliquestudios.com>
2017-04-06 10:59:22 -0500

    add default host to devise config??

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'sts@askagainlater.com'
+  config.default_url_options = { :host => 'characters.askagainlater.com' }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
