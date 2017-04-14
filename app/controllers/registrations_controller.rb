487131b7090cf2637f6b7cd65133f355b6ec1946
cmho <carlymho@gmail.com>
2017-04-13 22:49:30 -0500

    add after_inactive_sign_up_path_for method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,9 @@ class RegistrationsController < Devise::RegistrationsController
     flash[:alert] = i18n.t('en.devise.confirmations.send_instructions')
     redirect_to new_user_session_path
   end
+
+  def after_inactive_sign_up_path_for(resource)
+    flash[:alert] = i18n.t('en.devise.confirmations.send_instructions')
+    redirect_to new_user_session_path
+  end
 end
