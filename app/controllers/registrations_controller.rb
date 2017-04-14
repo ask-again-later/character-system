5ecb8321f82a698e48d7cfff975df70c4b586fcd
cmho <carlymho@gmail.com>
2017-04-13 22:37:43 -0500

    spruce up notifications a little

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    flash[:alert] = i18n.t('en.devise.confirmations.send_instructions')
+    redirect_to new_user_session_path
+  end
+end
