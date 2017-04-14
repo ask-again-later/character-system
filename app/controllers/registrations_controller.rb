f6082fc19da8ebbc796e7bca7727f9a8ad018ff4
cmho <carlymho@gmail.com>
2017-04-13 23:39:58 -0500

    fix I18n call and translation path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
-    flash[:alert] = i18n.t('en.devise.confirmations.send_instructions')
-    redirect_to new_user_session_path
-  end
-
   def after_inactive_sign_up_path_for(resource)
-    flash[:alert] = i18n.t('en.devise.confirmations.send_instructions')
+    flash[:alert] = I18n.t 'devise.confirmations.send_instructions'
     redirect_to new_user_session_path
   end
 end
