da28e966bd0e7d40837b927c8f452793b1ef748b
cmho <carlymho@gmail.com>
2017-04-30 15:12:30 -0500

    fix experience calculator when there's no advantages change

--- app/models/xp_expenditure.rb
+++ app/models/xp_expenditure.rb
@@ -35,30 +35,32 @@ class XpExpenditure < ApplicationRecord
 			end
 		end
 
-		advantages_old = diff['character_has_advantages'][0]
-		advantages_new = diff['character_has_advantages'][1]
+		if diff['character_has_advantages'].present?
+			advantages_old = diff['character_has_advantages'][0]
+			advantages_new = diff['character_has_advantages'][1]
 
-		advantages_removed = advantages_old.select{|ao| advantages_new.index{|an| an['id'] == ao['id']}.nil?}
-		advantages_updated = advantages_old.reject{|ao| ab = advantages_new.bsearch{|an| an['id'] == ao['id']}; ab.present? ? ab['rating'] == ao['rating'] : true}
-		advantages_added = advantages_new.select{|an| an['id'].nil?}
+			advantages_removed = advantages_old.select{|ao| advantages_new.index{|an| an['id'] == ao['id']}.nil?}
+			advantages_updated = advantages_old.reject{|ao| ab = advantages_new.bsearch{|an| an['id'] == ao['id']}; ab.present? ? ab['rating'] == ao['rating'] : true}
+			advantages_added = advantages_new.select{|an| an['id'].nil?}
 
-		advantages_updated.each do |au|
-			oldval = advantages_old.bsearch{|ao| ao['id'] == au['id']}
-			newval = advantages_new.bsearch{|an| an['id'] == au['id']}
-			if oldval.nil?
-				diffval = newval['rating']
-			else
-				diffval = newval['rating'] - oldval['rating']
+			advantages_updated.each do |au|
+				oldval = advantages_old.bsearch{|ao| ao['id'] == au['id']}
+				newval = advantages_new.bsearch{|an| an['id'] == au['id']}
+				if oldval.nil?
+					diffval = newval['rating']
+				else
+					diffval = newval['rating'] - oldval['rating']
+				end
+				count += diffval
 			end
-			count += diffval
-		end
 
-		advantages_removed.each do |ar|
-			count -= ar['rating']
-		end
+			advantages_removed.each do |ar|
+				count -= ar['rating']
+			end
 
-		advantages_added.each do |aa|
-			count += aa['rating']
+			advantages_added.each do |aa|
+				count += aa['rating']
+			end
 		end
 
 		self.amount = count
