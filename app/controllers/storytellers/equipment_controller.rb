fdb7c5286c2dfe8d23d8bdaea022b72b7cc4e378
cmho <carlymho@gmail.com>
2017-05-07 11:31:01 -0500

    implement controller methods

---
+++ app/controllers/storytellers/equipment_controller.rb
@@ -0,0 +1,58 @@
+module Storyteller
+  class EquipmentController < ApplicationController
+    before_action :authenticate_user!, :requires_storyteller
+
+    add_breadcrumb "Storytellers", :storytellers_path
+    add_breadcrumb "Equipment", :storytellers_equipment_path
+
+    def index
+      @equipment = Equipment.all
+    end
+
+    def show
+      @equipment = Equipment.find(params[:id])
+    end
+
+    def new
+      @equipment = Equipment.new
+    end
+
+    def create
+      @equipment = Equipment.new(equipment_params)
+      if @equipment.save
+        flash[:success] = "Your equipment was saved."
+        redirect_to storytellers_equipment_path and return
+      else
+        flash[:error] = "There was an error saving your equipment."
+        redirect_to new_storytellers_equipment_path and return
+      end
+    end
+
+    def edit
+      @equipment = Equipment.find(params[:id])
+    end
+
+    def update
+      @equipment = Equipment.find(params[:id])
+      if @equipment.update_attributes(equipment_params)
+        flash[:success] = "Your equipment was saved."
+        redirect_to storytellers_equipment_path and return
+      else
+        flash[:error] = "There was an error saving your equipment."
+        redirect_to edit_storytellers_equipment_path(@equipment) and return
+      end
+    end
+
+    def destroy
+      Equipment.destroy(params[:id])
+      flash[success] = "Your equipment was destroyed."
+      redirect_to storytellers_equipment_path and return
+    end
+
+    private
+
+    def equipment_params
+      params.require(:equipment).permit(:id, :name, :description, :equipment_type_id, {:character_ids => []})
+    end
+  end
+end
