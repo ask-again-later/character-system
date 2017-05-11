bbf2a410005af9620255a85630a1b750c86a2bf2
cmho <carlymho@gmail.com>
2017-05-10 19:06:39 -0500

    Add responsive plugin to datatables and pass params to some large tables that need it

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  $('.table').dataTable();
+  $('.table').dataTable({
+    responsive: {
+      details: {
+        type: '',
+        display: $.fn.dataTable.Responsive.display.childRowImmediate,
+      }
+    }
+  });
 
   $('textarea').wrap('<div class="textarea-wrapper"></div>');
   $('.textarea-wrapper').append('<span class="notice">You can use <a href="https://guides.github.com/features/mastering-markdown/" rel="external" target="_blank">Markdown</a> in this area.</span>');
