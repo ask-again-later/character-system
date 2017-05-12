1d47305345f515f08e420eaa55ceecfe9899565e
cmho <carlymho@gmail.com>
2017-05-11 20:26:42 -0500

    Add conditional paging plugin when paging is unnecessary

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  $('.table').dataTable();
+  $('.table').dataTable({
+    conditionalPaging: true
+  });
 
   $('textarea').wrap('<div class="textarea-wrapper"></div>');
   $('.textarea-wrapper').append('<span class="notice">You can use <a href="https://guides.github.com/features/mastering-markdown/" rel="external" target="_blank">Markdown</a> in this area.</span>');
